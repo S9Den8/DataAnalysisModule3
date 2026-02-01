57fdad0aa77986acbcc2a3a4115514f87615ad06
S9Den8 <nnsabatini.89@gmail.com>
2026-02-01 01:28:09 -0700

    Week 1 SQL basics assignment

--- Week1/AssignmentFiles/SQL_basics.sql
+++ Week1/AssignmentFiles/SQL_basics.sql
@@ -7,24 +7,62 @@ USE coffeeshop_db;
 -- =========================================================
 
 -- Q1) List all products (show product name and price), sorted by price descending.
+SELECT name, price
+FROM products
+ORDER BY price DESC;
 
 -- Q2) Show all customers who live in the city of 'Lihue'.
+SELECT *
+FROM customers 
+WHERE city = 'Lihue';
 
 -- Q3) Return the first 5 orders by earliest order_datetime (order_id, order_datetime).
+SELECT order_id, order_datetime
+FROM orders
+ORDER BY order_datetime ASC
+LIMIT 5;
 
 -- Q4) Find all products with the word 'Latte' in the name.
+SELECT *
+FROM products 
+WHERE name LIKE '%Latte%';
 
 -- Q5) Show distinct payment methods used in the dataset.
+SELECT DISTINCT payment_method
+FROM orders;
 
 -- Q6) For each store, list its name and city/state (one row per store).
+SELECT name, city, state
+FROM stores;
 
 -- Q7) From orders, show order_id, status, and a computed column total_items
---     that counts how many items are in each order.
+--     that counts how many items are in each order. ***Notes: "computed column total_items" doesn't exist, must create. 
+SELECT 
+	o.order_id, 	-- o alias comes from "orders", this needed to be created
+	o.status, 
+    SUM(oi.quantity) AS total_items -- oi = computed from order_items, o = originates from orders
+FROM orders o
+JOIN order_items oi
+	ON o.order_id = oi.order_id
+GROUP BY o.order_id, o.status
+ORDER BY o.order_id;
+
 
 -- Q8) Show orders placed on '2025-09-04' (any time that day).
+SELECT *
+FROM orders
+WHERE order_datetime >= '2025-09-04'
+	AND order_datetime < '2025-09-05';
 
 -- Q9) Return the top 3 most expensive products (price, name).
+SELECT price, name
+FROM products
+ORDER BY price DESC
+LIMIT 3;
 
 -- Q10) Show customer full names as a single column 'customer_name'
 --      in the format "Last, First".
+SELECT CONCAT(last_name, ', ', first_name) AS customer_name
+FROM customers
+ORDER BY last_name, first_name;
 
